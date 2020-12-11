0ad6c7b261ee63f09fcc2015e290bfdfe108b869
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-12-11 20:35:24 +0100

    SearchBar edited. It needs to be tested yet

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import { withAuth } from './../../context/auth-context';
 import './SearchBar.css'
 
 function SearchBar() {
     return (
-        <div class="wrap">
-            <div class="search">
-                <input type="text" class="searchTerm" placeholder="Search for the hotel" />
-                <button type="submit" class="searchButton">
-                    <i class="fa fa-search"></i>
+        <div className="wrap">
+            <div className="search">
+                <input type="text" className="searchTerm" placeholder="Search for the hotel" />
+                <button type="submit" className="searchButton">
+                    <i className="fa fa-search"></i>
                 </button>
+                <div className="dropdown">
+                    <button className="dropbtn">District</button>
+                    {props.user.district.map((district) => {
+
+                        <div key={district._id} district={district} className="dropdown-content" />
+                    })
+                    }
+                    <button className="dropbtn">Category</button>
+                    {props.user.category.map((category) => {
+
+                        <div key={category._id} district={category} className="dropdown-content" />
+                    })
+                    }
+                </div>
             </div>
         </div>
     )
 }
 
-export default SearchBar;
+export default withAuth(SearchBar)
