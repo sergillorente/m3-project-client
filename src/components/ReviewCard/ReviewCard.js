9fc3ec2b3b2d8299cbdb99388d27a258afe36d35
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-12-16 18:09:28 +0100

    New library added, moment. Time updated and looking better with this library

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withAuth } from '../../context/auth-context'
+import moment from 'moment'
 
 import './ReviewCard.css'
 
@@ -19,7 +20,7 @@ const ReviewCard = (props) => {
     <div className="review">
         <img className="picture" src='/images/default-profile.jpg' alt='user' /> {/* Backlog --- Add the picture of every single user who updates its picture profile // userId.picture */}
         <p>{userId.username}</p>
-        <p>{created_at}</p>
+        <p>{moment(created_at).format('YYYY-MM-DD hh:mm:ss a')}</p>
         <p>{text}</p>
         <p>{rating}</p>
         {userId === currentUserId && <button id="delete-btn" onClick={(event) => handleDelete(event, reviewId, index)} >Delete</button>}
